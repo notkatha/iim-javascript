8af6f303c6e4f5df55b65120dcb5d4b15775815b
katha <ewan.girard-hennebuisse@edu.devinci.fr>
2024-01-29 11:00:57 +0100

    Create script.js

---
+++ exercices/script.js
@@ -0,0 +1,13 @@
+const lastname = "Girard"
+const firstname = "Ewan"
+let age = 18;
+
+console.log(
+    "Yo bg, moi c'est " +
+    firstname + 
+    " " +
+    lastname + 
+    " et j'ai" +
+    age + 
+    " ballets."
+);
